29e2a9daed5a74f2c078604d78e2e75b848802b6
griselizr <griseliz.r.c15@gmail.com>
2022-09-02 05:00:02 +0000

    remove tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete the complete binary tree
+ * @tree: the tree
+ * Return: if tree is null
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
+}
