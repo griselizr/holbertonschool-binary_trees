643e24305f78b842f0b0a7b79d9c1b75ffc4cf81
griselizr <griseliz.r.c15@gmail.com>
2022-09-04 02:49:20 +0000

    new file

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_nodes - counts nodes
+ *@tree: root node of the tree to count the number of nodes
+ *Return: size or 0
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (0);
+}
